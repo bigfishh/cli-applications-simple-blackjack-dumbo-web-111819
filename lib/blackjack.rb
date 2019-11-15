a257bdf466ec5b200158b9c543d3bfdad9ad5c88
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-15 04:39:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,7 @@ def hit?(current_sum)
   # code hit? here
   prompt_user
   input = get_user_input
-  if input == 'h'
+  if get_user_input == 'h'
     current_sum += deal_card
   elsif input == 's'
     current_sum
