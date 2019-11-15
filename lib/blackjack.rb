e5317ffdff7e77443e94281549c188399f02c5ae
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-15 04:20:48 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -43,8 +43,10 @@ def hit?(num)
   get_user_input
   if get_user_input == 'h'
     deal_card
-  else get_user_input != 's' && get_user_input != 'h'
+  elsif get_user_input != 's' && get_user_input != 'h'
     invalid_command
+  else 
+    prompt_user
   end
   prompt_user
 end
